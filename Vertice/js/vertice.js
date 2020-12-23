0cb08e0a3a8b1f47b2b839896b3f6146b83d802f
Richie_Sjt <richi252012@hotmail.com>
2020-12-22 20:20:30 -0600

    Carouseles
    Co-Authored-By: Eduardo Herrera <33185685+EduardoHerreraJ@users.noreply.github.com>

--- Vertice/js/vertice.js
+++ Vertice/js/vertice.js
@@ -1,16 +1,32 @@
-$('#carouselConoce').carousel({
-    interval: 2000
+//Configuración inicial de los carouseles
+$('.carouselNoModal').carousel({
+    interval: 3500
 })
-
-$('#carouselConoce').click(function () {
-    pauseCarousel();
-}); 
-
-$('#carouselModalConoce').carousel({
+$('.carouselModal').carousel({
     interval: false
 })
 
-function pauseCarousel(){
-    $('#carouselConoce').carousel('pause');
-    console.log('se ha pausado el carousel');
-}
+$('.modal').on('show.bs.modal', function (e) {
+    //Cuando ocurre un evento de abrir un carousel
+    //Mostrar modal de imagen de carousel pantalla pequeña en el índice actual
+    if(screen.width < 993){
+        const nameHashtag = '#carousel' + this.dataset.target.slice(6);
+        const name = this.dataset.target.substring(1);
+        const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+        let carouselTarget = '#carousel' + nameCapitalized;
+        $(carouselTarget).carousel($(nameHashtag).find('.active').index());
+    }
+})
+
+$('.carousel-trigger').on('click', function(){
+    //Cuando se clickea una imagen de la card
+    //Mostrar modal de imagen de carousel pantalla grande en el índicie actual
+    if(screen.width > 992){
+        let current = parseInt(this.dataset.slideTo);
+        const name = this.dataset.target.substring(1);
+        const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+        let carouselTarget = '#carousel' + nameCapitalized;
+        //seguir el formato '#carouselModalName'
+        $(carouselTarget).carousel(current);
+    }
+});
